1a3e5be09f3fa7ab0b4e8b44050e6ffbe87782d4
Simon <Simon@imac-de-maison.home>
2018-05-05 00:44:43 +0200

    carousel operationel

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -13,15 +13,29 @@ class Carousel {
     this.container = this.createDivWithClass('carousel-container')
     this.carousel.appendChild(this.container)
     this.element.appendChild(this.carousel)
+    let num = 0
     this.items = children.map((child) => {
       let item = this.createDivWithClass('carousel-item')
+      item.setAttribute('number', num + '')
+      num++
       item.appendChild(child)
       this.container.appendChild(item)
       return item
     })
 
+    this.items.forEach(item => item.addEventListener('click', function () {
+      document.querySelector('.carousel-hover.hidden').classList.remove('hidden')
+      let number = parseInt(item.getAttribute('number'))
+      carouselHover.goToItem(number)
+    }))
+
     this.setStyle()
     this.createNavigation()
+
+    let carouselHover = new CarouselHover(document.querySelector('body'), {
+      slidesToScroll : 1,
+      slidesVisible  : 1
+    })
 
   }
 
@@ -78,15 +92,99 @@ class Carousel {
 
 }
 
+class CarouselHover {
+
+  constructor(element, options = {}) {
+
+    this.element = element
+    this.options = options
+    this.carouselHover = this.createDivWithClass('carousel-hover')
+    this.carouselHover.classList.add('hidden')
+    this.currentItem = 0
+    element.appendChild(this.carouselHover)
+    this.carouselHoverContainer = this.createDivWithClass('carousel-hover-container')
+    let carouselContainer = document.querySelector('.carousel-container').cloneNode(true)
+    let children = [].slice.call(carouselContainer.children)
+    this.items = children.map((child) => {
+      child.classList.remove('carousel-item')
+      child.classList.add('carousel-hover-item')
+      this.carouselHoverContainer.appendChild(child)
+      return child
+    })
+    this.carouselHover.appendChild(this.carouselHoverContainer)
+
+    this.createNavigation()
+    this.setStyle()
+
+  }
+
+  createDivWithClass (className) {
+
+    let div = document.createElement('div')
+    div.classList.add(className)
+    return div
+
+  }
+
+  createNavigation () {
+
+    let previousButton = this.createDivWithClass('carousel-hover-button')
+    previousButton.classList.add('carousel-hover-button-previous')
+    let nextButton = this.createDivWithClass('carousel-hover-button')
+    nextButton.classList.add('carousel-hover-button-next')
+    let returnButton = this.createDivWithClass('carousel-hover-return')
+    this.carouselHover.appendChild(previousButton)
+    this.carouselHover.appendChild(nextButton)
+    this.carouselHover.appendChild(returnButton)
+    previousButton.addEventListener('click', this.previous.bind(this))
+    nextButton.addEventListener('click', this.next.bind(this))
+    returnButton.addEventListener('click', () => {
+      this.carouselHover.classList.add('hidden')
+    })
+
+  }
+
+  setStyle () {
+
+      let ratio = this.items.length / this.options.slidesVisible
+      this.carouselHoverContainer.style.width = (ratio * 100) + '%'
+      this.items.forEach(item => item.style.width = ((100 / this.options.slidesVisible) / ratio) + '%')
+
+  }
+
+  next () {
+    this.goToItem(this.currentItem + this.options.slidesToScroll)
+  }
+
+  previous () {
+    this.goToItem(this.currentItem - this.options.slidesToScroll)
+  }
+
+  goToItem (index) {
+
+    if (index < 0) {
+      index = this.items.length - this.options.slidesVisible
+    } else if (index >= this.items.length) {
+      index = 0
+    }
+    let translate = index * (-100 / this.items.length)
+    this.carouselHoverContainer.style.transform = 'translate3d(' + translate + '%, 0, 0)'
+    this.currentItem = index
+
+  }
+
+}
+
 document.addEventListener('DOMContentLoaded', function () {
 
   new Carousel(document.querySelector('#portfolio'), {
-    slidesToScroll: 3,
-    slidesVisible : 3
+    slidesToScroll : 3,
+    slidesVisible  : 3
   })
 
   let voirPlus = document.querySelector('#voir-plus')
   let returnPresentation = document.querySelector('#return-presentation')
+  let navbar = document.querySelector('#mainMenu')
 
   voirPlus.addEventListener('click', function () {
     document.querySelector('#under-presentation').classList.add('clicked')
@@ -95,6 +193,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
   returnPresentation.addEventListener('click', function () {
     document.querySelector('#under-presentation').classList.remove('clicked')
+    voirPlus.style.display = "inline"
+  })
+
+  window.addEventListener('scroll', function () {
+    if (window.scrollY >= 200) {
+      navbar.classList.add('scrolled')
+    } else {
+      navbar.classList.remove('scrolled')
+    }
   })
 
 })
